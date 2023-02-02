14e55259efe6a83dbeb8ce0f8768f625aa5185ec
Md.Nurnabi Miah <73682576+nurnabimiah@users.noreply.github.com>
2023-02-02 12:52:40 +0600

    Delete task_4_todo directory

--- task_4_todo/android/app/src/main/kotlin/com/example/task_4_todo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.task_4_todo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- task_4_todo/lib/models/contact.dart
+++
@@ -1,5 +0,0 @@
-class ContactModel {
-  String name;
-  String contact;
-  ContactModel({required this.name, required this.contact});
-}
